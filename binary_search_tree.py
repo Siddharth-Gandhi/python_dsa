c419ed7d8d55f84b2946fabd56b982621877b776
rub1cks <ssgandhi602@gmail.com>
2020-09-07 20:01:01 +0530

    starting out

---
+++ binary_search_tree.py
@@ -0,0 +1,9 @@
+# LOWER LEVEL PROTOCOL:
+# each node has at max 2 nodes
+# meaning local constraints
+
+# HIGHER LEVEL PROTOCOL:
+# "search" every node to the left of a node has values <
+# the current node value. So the constraint isn't just on one node but
+# multiple nodes at the same time
+
